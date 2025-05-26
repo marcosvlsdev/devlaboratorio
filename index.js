acf22c932444ed2bbb7d47e2604b8ee2a6ee9ccf
Marcos Vinicius <158005194+marcosvlsdev@users.noreply.github.com>
2025-05-26 00:21:40 -0300

    feat: add color and image changes when toogle the modes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const alterarModo = document.getElementById ("modo");
 const body  = document.querySelector("body");
 const imagemModo = document.querySelector(".imagem-botao");
+const imagemLogo = document.querySelector(".logo")
+const imagemLogoFooter = document.querySelector(".logo-footer")
 
 alterarModo.addEventListener("click", () => {
     const modoEscuroAtivo = body.classList.contains("modo-escuro");
@@ -9,8 +11,12 @@ alterarModo.addEventListener("click", () => {
 
     if (modoEscuroAtivo) {
         imagemModo.setAttribute("src", "img/sun.png")
+        imagemLogo.setAttribute("src", "img/lab-removebg-preview.png")
+        imagemLogoFooter.setAttribute("src", "img/lab-removebg-preview.png")
     } else {
         imagemModo.setAttribute("src", "img/lua1.png")
+        imagemLogo.setAttribute("src", "img/lab-claro.png")
+        imagemLogoFooter.setAttribute("src", "img/lab-claro.png")
     }
 
     
